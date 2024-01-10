b6cd32188794de015ff94d4ed339ae8ff91ec659
Neti <98841356+Netidion@users.noreply.github.com>
2024-01-10 15:30:18 +0200

    Adding Cart and Payment status enums

---
+++ src/main/java/com/vnet/lab/utils/enums/CartStatus.java
@@ -0,0 +1,6 @@
+package com.vnet.lab.utils.enums;
+
+public enum CartStatus {
+
+    NEW, CANCELED, CONFIRMED
+}
---
+++ src/main/java/com/vnet/lab/utils/enums/PaymentStatus.java
@@ -0,0 +1,5 @@
+package com.vnet.lab.utils.enums;
+
+public enum PaymentStatus {
+    ACCEPTED, PENDING, REFUSED, ERROR
+}
