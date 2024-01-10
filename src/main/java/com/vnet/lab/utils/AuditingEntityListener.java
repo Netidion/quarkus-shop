56e4843729d4745ebda6a91abeab4c53ba7109f8
Neti <98841356+Netidion@users.noreply.github.com>
2024-01-10 14:06:38 +0200

    Adding AuditingEntityListener

---
+++ src/main/java/com/vnet/lab/utils/AuditingEntityListener.java
@@ -0,0 +1,23 @@
+package com.vnet.lab.utils;
+
+import com.vnet.lab.entity.AbstractEntity;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+
+import java.time.Instant;
+
+public class AuditingEntityListener {
+
+    @PrePersist
+    void preCreate(AbstractEntity auditable) {
+        Instant now = Instant.now();
+        auditable.setCreatedDate(now);
+        auditable.setLastModified(now);
+    }
+
+    @PreUpdate
+    void preUpdate(AbstractEntity auditable) {
+        Instant now = Instant.now();
+        auditable.setLastModified(now);
+    }
+}
