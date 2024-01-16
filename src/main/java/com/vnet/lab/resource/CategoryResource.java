e8d2487a1bf61c82f8755255a4c60a95c8ea8b1b
Neti <98841356+Netidion@users.noreply.github.com>
2024-01-16 18:05:28 +0200

    Adding Web layer for Category

---
+++ src/main/java/com/vnet/lab/resource/CategoryResource.java
@@ -0,0 +1,68 @@
+package com.vnet.lab.resource;
+
+import com.vnet.lab.dto.CategoryDto;
+import com.vnet.lab.dto.ProductDto;
+import com.vnet.lab.service.CategoryService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+
+import java.util.List;
+
+@Path("/categories")
+public class CategoryResource {
+
+    @Inject
+    CategoryService categoryService;
+
+    /**
+     * Listing all categories: HTTP GET on path /api/categories
+     * @return CategoryDto
+     */
+    @GET
+    public List<CategoryDto> findAll(){
+        return this.categoryService.findAll();
+    }
+
+    /**
+     *
+     * Retrieving category for a given category id: HTTP GET on path /api/categories/{id}
+     * @param id categoryId
+     * @return CategoryDto
+     */
+    @GET
+    @Path("/{id}")
+    public CategoryDto findById(@PathParam("id") Long id) {
+        return this.categoryService.findById(id);
+    }
+
+    /**
+     * Listing products for a given category id: HTTP GET on path /api/categories/{id}/products
+     * @param id categoryId
+     * @return ProductDto
+     */
+    @GET
+    @Path("/{id}/products")
+    public List<ProductDto> findProductsByCategoryId(@PathParam("id") Long id){
+        return this.categoryService.findProductsByCategoryId(id);
+    }
+
+    /**
+     * Creating a new category: HTTP POST on path /api/categories
+     * @param categoryDto new category to be inserted
+     * @return CategoryDto new category inserted
+     */
+    @POST
+    public CategoryDto create(CategoryDto categoryDto){
+        return this.categoryService.create(categoryDto);
+    }
+
+    /**
+     * Deleting category for a given category id: HTTP DELETE on path /api/categories/{id}
+     * @param id category id
+     */
+    @DELETE
+    @Path("/{id}")
+    public void delete(@PathParam("id") Long id){
+        this.categoryService.delete(id);
+    }
+}
