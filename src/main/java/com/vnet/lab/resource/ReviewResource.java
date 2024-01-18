e41996940d2a8b91e3c4f55e11946a1004eb6a7f
Neti <98841356+Netidion@users.noreply.github.com>
2024-01-18 16:53:41 +0200

    Adding Web layer for Review

---
+++ src/main/java/com/vnet/lab/resource/ReviewResource.java
@@ -0,0 +1,60 @@
+package com.vnet.lab.resource;
+
+import com.vnet.lab.dto.ReviewDto;
+import com.vnet.lab.service.ReviewService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+
+import java.util.List;
+
+@Path("/reviews")
+public class ReviewResource {
+
+    @Inject
+    ReviewService reviewService;
+
+    /**
+     * Listing all reviews regarding the product with given id: HTTP GET on path /api/reviews/product/{id}
+     * @param id productId
+     * @return List of ReviewDto
+     */
+    @GET
+    @Path("/product/{id}")
+    public List<ReviewDto> findAllByProductId(@PathParam("id") Long id){
+        return this.reviewService.findReviewsByProductId(id);
+    }
+
+    /**
+     * Retrieving review with given id: HTTP GET on path /api/reviews/{id}
+     * @param id reviewId
+     * @return ReviewDto
+     */
+    @GET
+    @Path("/{id}")
+    public ReviewDto findById(@PathParam("id") Long id) {
+        return this.reviewService.findById(id);
+    }
+
+    /**
+     * Creating a review for a product with given id: HTTP POST on path /api/reviews/product/{id}
+     * @param reviewDto review to be created
+     * @param id productId
+     * @return ReviewDto review created
+     */
+    @POST
+    @Path("/product/{id}")
+    public ReviewDto create(ReviewDto reviewDto, @PathParam("id") Long id){
+        return this.reviewService.create(reviewDto, id);
+    }
+
+    /**
+     * Deleting a review with given id: HTTP POST on path /api/reviews/{id}
+     * @param id reviewId
+     */
+    @DELETE
+    @Path("{id}")
+    public void delete(@PathParam("id") Long id){
+        this.reviewService.delete(id);
+    }
+
+}
