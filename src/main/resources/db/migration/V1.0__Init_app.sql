d75758a9571fdb2698e8c1dda612152d40f18d30
Neti <98841356+Netidion@users.noreply.github.com>
2024-03-17 19:44:57 +0200

    Adding migration scripts

--- src/main/resources/db/migration/V1.0__Init_app.sql
+++ src/main/resources/db/migration/V1.0__Init_app.sql
@@ -1,19 +1,136 @@
-create table abstract_entities (id serial primary key, created_date timestamp(6) with time zone not null, last_modified timestamp(6) with time zone);
-create table customers (id bigint not null, created_date timestamp(6) with time zone not null, last_modified timestamp(6) with time zone, email varchar(255), enabled boolean not null, first_name varchar(255), last_name varchar(255), telephone varchar(255), primary key (id));
-create table carts (id bigint not null, created_date timestamp(6) with time zone not null, last_modified timestamp(6) with time zone, customer_id bigint, status varchar(255) not null, primary key (id));
-create table order_items (id bigint not null, created_date timestamp(6) with time zone not null, last_modified timestamp(6) with time zone, quantity bigint not null, order_id bigint, product_id bigint, primary key (id));
-create table orders (id bigint not null, created_date timestamp(6) with time zone not null, last_modified timestamp(6) with time zone, total_price numeric(10,2) not null, address_1 varchar(255), address_2 varchar(255), city varchar(255), country varchar(2) not null, post_code varchar(10) not null, shipped timestamp(6) with time zone, status varchar(255) not null check (status in ('CREATION','PAID','SHIPPED','DELIVERED','CLOSED')), cart_id bigint, payment_id bigint, primary key (id));
-create table payments (id bigint not null, created_date timestamp(6) with time zone not null, last_modified timestamp(6) with time zone, amount numeric(38,2) not null, e_payment_id varchar(255), status varchar(255) check (status in ('ACCEPTED','PENDING','REFUSED','ERROR')), primary key (id));
-create table products (id bigint not null, created_date timestamp(6) with time zone not null, last_modified timestamp(6) with time zone, description varchar(255) not null, name varchar(255) not null, price numeric(10,2) not null, product_status smallint not null check (product_status between 0 and 1), sales_counter integer, category_id bigint, primary key (id));
-create table categories (id bigint not null, created_date timestamp(6) with time zone not null, last_modified timestamp(6) with time zone, description varchar(255) not null, name varchar(255) not null, primary key (id));
-create table products_reviews (product_id bigint not null, reviews_id bigint not null, primary key (product_id, reviews_id));
-create table reviews (id bigint not null, created_date timestamp(6) with time zone not null, last_modified timestamp(6) with time zone, description varchar(255) not null, rating bigint not null, title varchar(255) not null, primary key (id));
+create sequence hibernate_sequence START WITH 100 INCREMENT BY 1;
 
-create sequence customers_SEQ start with 1 increment by 50;
-create sequence carts_SEQ start with 1 increment by 50;
-create sequence categories_SEQ start with 1 increment by 50;
-create sequence order_items_SEQ start with 1 increment by 50;
-create sequence orders_SEQ start with 1 increment by 50;
-create sequence payments_SEQ start with 1 increment by 50;
-create sequence products_SEQ start with 1 increment by 50;
-create sequence reviews_SEQ start with 1 increment by 50;
+create table "carts"
+(
+    "id"                 bigint       not null,
+    "created_date"       timestamp    not null,
+    "last_modified_date" timestamp,
+    "status"             varchar(255) not null,
+    "customer_id"        bigint
+);
+alter table "carts"
+    add constraint "cart_pk" primary key ("id");
+
+create table "categories"
+(
+    "id"                 bigint       not null,
+    "created_date"       timestamp    not null,
+    "last_modified_date" timestamp,
+    "description"        varchar(255) not null,
+    "name"               varchar(255) not null
+);
+alter table "categories"
+    add constraint "categories_pk" primary key ("id");
+
+create table "customers"
+(
+    "id"                 bigint    not null,
+    "created_date"       timestamp not null,
+    "last_modified_date" timestamp,
+    "email"              varchar(255),
+    "enabled"            boolean   not null,
+    "first_name"         varchar(255),
+    "last_name"          varchar(255),
+    "telephone"          varchar(255)
+);
+alter table "customers"
+    add constraint "customer_pk" primary key ("id");
+
+create table "order_items"
+(
+    "id"                 bigint    not null,
+    "created_date"       timestamp not null,
+    "last_modified_date" timestamp,
+    "quantity"           bigint    not null,
+    "order_id"           bigint,
+    "product_id"         bigint
+);
+alter table "order_items"
+    add constraint "order_item_pk" primary key ("id");
+
+create table "orders"
+(
+    "id"                 bigint         not null,
+    "created_date"       timestamp      not null,
+    "last_modified_date" timestamp,
+    "address_1"          varchar(255),
+    "address_2"          varchar(255),
+    "city"               varchar(255),
+    "country"            varchar(2),
+    "postcode"           varchar(10),
+    "shipped"            timestamp,
+    "status"             varchar(255)   not null,
+    "total_price"        decimal(10, 2) not null,
+    "cart_id"            bigint,
+    "payment_id"         bigint
+);
+alter table "orders"
+    add constraint "orders_pk" primary key ("id");
+
+create table "payments"
+(
+    "id"                 bigint         not null,
+    "created_date"       timestamp      not null,
+    "last_modified_date" timestamp,
+    "paypal_payment_id"  varchar(255),
+    "status"             varchar(255)   not null,
+    "amount"             decimal(10, 2) not null
+);
+alter table "payments"
+    add constraint "payment_pk" primary key ("id");
+
+create table "products"
+(
+    "id"                 bigint         not null,
+    "created_date"       timestamp      not null,
+    "last_modified_date" timestamp,
+    "description"        varchar(255)   not null,
+    "name"               varchar(255)   not null,
+    "price"              decimal(10, 2) not null,
+    "sales_counter"      integer,
+    "status"             varchar(255)   not null,
+    "category_id"        bigint
+);
+alter table "products"
+    add constraint "product_pk" primary key ("id");
+
+create table "products_reviews"
+(
+    "product_id" bigint not null,
+    "reviews_id" bigint not null
+);
+alter table "products_reviews"
+    add constraint "products_reviews_pk" primary key ("product_id", "reviews_id");
+
+create table "reviews"
+(
+    "id"                 bigint       not null,
+    "created_date"       timestamp    not null,
+    "last_modified_date" timestamp,
+    "description"        varchar(255) not null,
+    "rating"             bigint       not null,
+    "title"              varchar(255) not null
+);
+alter table "reviews"
+    add constraint "review_pk" primary key ("id");
+
+alter table "products_reviews"
+    add constraint "products_reviews_uk" unique ("reviews_id");
+alter table "orders"
+    add constraint "orders_uk" unique ("payment_id");
+alter table "products_reviews"
+    add constraint "products_reviews_fk1" foreign key ("reviews_id") references "reviews" ("id");
+alter table "carts"
+    add constraint "cart_fk" foreign key ("customer_id") references "customers" ("id");
+alter table "order_items"
+    add constraint "order_item_fk1" foreign key ("product_id") references "products" ("id");
+alter table "orders"
+    add constraint "orders_fk1" foreign key ("payment_id") references "payments" ("id");
+alter table "order_items"
+    add constraint "order_item_fk2" foreign key ("order_id") references "orders" ("id");
+alter table "products"
+    add constraint "product_fk" foreign key ("category_id") references "categories" ("id");
+alter table "orders"
+    add constraint "orders_fk2" foreign key ("cart_id") references "carts" ("id");
+alter table "products_reviews"
+    add constraint "products_reviews_fk2" foreign key ("product_id") references "products" ("id");
--- src/main/resources/db/migration/V1.1__Insert_samples.sql
+++ src/main/resources/db/migration/V1.1__Insert_samples.sql
@@ -1,61 +1,60 @@
+insert into "categories"
+values (1, current_timestamp, current_timestamp, 'Phones & Smartphones', 'Mobile'),
+       (2, current_timestamp, current_timestamp, 'Computers and Laptops', 'PC');
 
--- Insert sample data into the 'customers' table
-INSERT INTO customers (id, created_date, last_modified, email, enabled, first_name, last_name, telephone)
-VALUES
-    (3, current_timestamp, current_timestamp, 'john.doe@example.com', true, 'John', 'Doe', '123456789'),
-    (4, current_timestamp, current_timestamp, 'jane.smith@example.com', true, 'Jane', 'Smith', '987654321'),
-    (5, current_timestamp, current_timestamp, 'max.black@example.com', true, 'Max', 'Black', '2569874563');
+insert into "products"
+values (1, current_timestamp, current_timestamp, 'The latest powerful iPhone from Apple', 'iPhone 11 Pro', 999.00, 0,
+        'AVAILABLE', 1),
+       (2, current_timestamp, current_timestamp, 'The most powerful iPhone from Apple', 'iPhone XS', 759.00, 0,
+        'AVAILABLE', 1),
+       (3, current_timestamp, current_timestamp, 'The most powerful MacBook from Apple', 'MacBook Pro 13', 1999.00, 0,
+        'AVAILABLE', 2),
+       (4, current_timestamp, current_timestamp, 'The phone that gets it done!', 'Google Pixel 4', 450.00, 0,
+        'AVAILABLE', 1);
 
--- Insert a record into the 'carts' table for initialization
-INSERT INTO carts (id, created_date, last_modified, customer_id, status)
-VALUES
-    (3,  current_timestamp, current_timestamp, 3, 'NEW'),
-    (4,  current_timestamp, current_timestamp, 4, 'NEW'),
-    (5,  current_timestamp, current_timestamp, 5, 'CANCELLED');
+insert into "reviews"
+values (1, current_timestamp, current_timestamp, 'I like the product but I found that it''s not perfect', 4,
+        'Good but not perfect'),
+       (2, current_timestamp, current_timestamp, 'Wonderful product', 5, 'Excellent'),
+       (3, current_timestamp, current_timestamp, 'I like the product but not the price', 4, 'Good but very expensive');
 
--- Insert sample data into the 'order_items' table
-INSERT INTO order_items (id, created_date, last_modified, quantity, order_id, product_id)
-VALUES
-    (1, current_timestamp, current_timestamp, 2, 1, 101),
-    (2, current_timestamp, current_timestamp, 1, 1, 102),
-    (3, current_timestamp, current_timestamp, 3, 2, 103);
+insert into "products_reviews"
+values (1, 1),
+       (1, 2),
+       (2, 3);
 
--- Insert sample data into the 'orders' table
-INSERT INTO orders (id, created_date, last_modified, total_price, address_1, address_2, city, country, post_code, shipped, status, cart_id, payment_id)
-VALUES
-    (1, current_timestamp, current_timestamp, 150.00, '123 Main St', 'Apt 45', 'Cityville', 'US', '12345', current_timestamp, 'PAID', 1, 101),
-    (2, current_timestamp, current_timestamp, 75.50, '456 Oak St', 'Suite 22', 'Townsville', 'US', '54321', current_timestamp, 'CREATION', 2, 102);
+insert into "customers"
+values (1, current_timestamp, current_timestamp, 'jason.bourne@mail.hello', TRUE, 'Jason', 'Bourne', '010203040506'),
+       (2, current_timestamp, current_timestamp, 'homer.simpson@mail.hello', TRUE, 'Homer', 'Simpson', '060504030201'),
+       (3, current_timestamp, current_timestamp, 'peter.quinn@mail.hello', FALSE, 'Peter', 'Quinn', '070605040302'),
+       (4, current_timestamp, current_timestamp, 'saul.berenson@mail.hello', FALSE, 'Saul', 'Berenson', '070503010204'),
+       (5, current_timestamp, current_timestamp, 'nebrass@quarkushop.store', TRUE, 'Nebrass', 'Lamouchi', '0606060606');
 
--- Insert sample data into the 'payments' table
-INSERT INTO payments (id, created_date, last_modified, amount, e_payment_id, status)
-VALUES
-    (101, current_timestamp, current_timestamp, 150.00, 'e123456', 'ACCEPTED'),
-    (102, current_timestamp, current_timestamp, 75.50, 'e789012', 'PENDING');
+insert into "carts"
+values (1, current_timestamp, current_timestamp, 'NEW', 1),
+       (2, current_timestamp, current_timestamp, 'NEW', 2),
+       (3, current_timestamp, current_timestamp, 'NEW', 3),
+       (4, current_timestamp, current_timestamp, 'NEW', 4),
+       (5, current_timestamp, current_timestamp, 'NEW', 5);
 
--- Insert sample data into the 'products' table
-INSERT INTO products (id, created_date, last_modified, description, name, price, product_status, sales_counter, category_id)
-VALUES
-    (101, current_timestamp, current_timestamp, 'Product 1 description', 'Product 1', 49.99, 1, 100, 201),
-    (102, current_timestamp, current_timestamp, 'Product 2 description', 'Product 2', 29.99, 1, 75, 202),
-    (103, current_timestamp, current_timestamp, 'Product 3 description', 'Product 3', 69.99, 0, 150, 203);
+insert into payments
+values (1, current_timestamp, current_timestamp, 'somePaymentId', 'ACCEPTED', 999.00),
+       (2, current_timestamp, current_timestamp, 'paymentId', 'ACCEPTED', 759.00),
+       (4, current_timestamp, current_timestamp, 'paymentId', 'ACCEPTED', 759.00);
 
--- Insert sample data into the 'customers' table
-INSERT INTO categories (id, created_date, last_modified, name, description)
-VALUES
-    (1, current_timestamp, current_timestamp, 'Category 1', 'Category 1 description'),
-    (2, current_timestamp, current_timestamp, 'Category 2', 'Category 2 description'),
-    (3, current_timestamp, current_timestamp, 'Category 3', 'Category 3 description');
+insert into "orders"
+values (1, current_timestamp, current_timestamp, '1 Rue Vaugirard', NULL, 'Paris', 'FR', '75015', NULL, 'PAID',
+        999.00, 1, 2),
+       (2, current_timestamp, current_timestamp, '2 Rue Maupertuis', NULL, 'Le Mans', 'FR', '72100', NULL, 'PAID',
+        759.00,
+        2, 1),
+       (3, current_timestamp, current_timestamp, '39 Quai du Président Roosevelt', NULL, 'Issy-les-Moulineaux', 'FR',
+        92130, NULL, 'CREATION', 0, 3, NULL),
+       (4, current_timestamp, current_timestamp, 'Cité Safia 2', NULL, 'Ksour', 'TN',
+        7160, NULL, 'CREATION', 0, 4, NULL),
+       (5, current_timestamp, current_timestamp, 'Cité Safia 2', NULL, 'Ksour', 'TN',
+        7160, NULL, 'CREATION', 0, 5, 4);
 
--- Insert sample data into the 'products_reviews' table
-INSERT INTO products_reviews (product_id, reviews_id)
-VALUES
-    (101, 1),
-    (101, 2),
-    (102, 3);
-
--- Insert sample data into the 'reviews' table
-INSERT INTO reviews (id, created_date, last_modified, description, rating, title)
-VALUES
-    (1, current_timestamp, current_timestamp, 'Great product!', 5, 'Excellent'),
-    (2, current_timestamp, current_timestamp, 'Fast shipping and good quality.', 4, 'Good'),
-    (3, current_timestamp, current_timestamp, 'Not satisfied with the product.', 2, 'Disappointing');
+insert into "order_items"
+values (1, current_timestamp, current_timestamp, 1, 1, 1),
+       (2, current_timestamp, current_timestamp, 1, 2, 2);
